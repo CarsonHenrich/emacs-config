10a97f59c6611a8aa22bb69af5bdbd2d8efd102a
Carson Henrich <carson03henrich@gmail.com>
2024-05-08 09:39:53 -0700

    Moving existing config to this repo

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d77d6ba33442dd3121b44e20af28f1fae8eeda413b2c3d3b9f1315fbda021992" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:family "Liga SFMono Nerd Font" :height 0.8))))
+ '(mode-line-inactive ((t (:family "Liga SFMono Nerd Font" :height 0.8))))
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
+(put 'magit-clean 'disabled nil)
