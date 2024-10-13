4e1fe261950b533c6643d69f8dba295ba1a688e5
Carson Henrich <carson03henrich@gmail.com>
2024-10-13 10:54:05 -0700

    Many changes

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((doom-user-dir . "/Users/carsonhenrich/.config/doom"))))
